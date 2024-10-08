0c0456a507a0644f95ff79d73115c099c61edcda
jalmen <xalmond@gmail.com>
2024-10-08 19:11:21 +0200

    lección 002

--- python_total/practica_02.py
+++ python_total/practica_02.py
@@ -1,7 +1,44 @@
-# strings
+# Variables
 
-print('Hola' + ' ' 'Fede')
-print('Me llamo \'Fede\'')
-print('Esta es una línea\nEsta es otra línea')
-print('\tEsta es otra más con sangría')
-print('Este caracter \\ es una barra invertida')
+nombre = 'Javier'
+print('Tu nombre es ' + nombre)
+
+# integers and floats
+
+numero = 5
+print(type(numero), numero, numero + numero)
+numero = 1.2
+print(type(numero), numero, numero + numero)
+numero = 5
+print(type(numero), type(numero*1.2), type(numero*1.0))
+
+# type of input
+
+edad = input('Dime tu edad: ')
+print(type(edad), edad + edad)
+
+num1 = 7.5
+num2 = 2.5
+print(type(num1+num2))
+
+# Conversiones
+
+numero_decimal = 5.7
+numero_entero = int(numero)
+print(numero_entero, type(numero_entero))
+
+nueva_edad = int(edad) + 1
+print('El año que viene tendrás ' + str(nueva_edad))
+
+# Formatear cadenas con format (old)
+x = 10
+y = 5
+print('Mis números son {} y {}'.format(x, y))
+print('La suma de {} y {} es {}'.format(x, y, x+y))
+
+# Formatear cadenas con cadenas literales (new from python 3.6)
+color = 'blanco'
+matricula = 5418
+print(f'Mi coche es {color} y con matrícula {matricula}')
+
+
