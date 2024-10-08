cfefeeafc27db6d57a065bff5f1843ac55fdb99f
jalmen <xalmond@gmail.com>
2024-10-08 12:54:37 +0200

    proyecto 01

---
+++ python_total/proyecto_01_01.py
@@ -0,0 +1,4 @@
+# print
+
+print('Hola Munddo, te digo "hola" = Hola Mundo, te digo \'hola\'')
+print('150 + 3 = ', 150 + 3)
---
+++ python_total/proyecto_01_02.py
@@ -0,0 +1,7 @@
+# strings
+
+print('Hola' + ' ' 'Fede')
+print('Me llamo \'Fede\'')
+print('Esta es una línea\nEsta es otra línea')
+print('\tEsta es otra más con sangría')
+print('Este caracter \\ es una barra invertida')
