6f22e1c47b0f2fa1655d011121f20e6d2b8b199e
jalmen <xalmond@gmail.com>
2024-10-18 09:16:22 +0200

    lección 006

--- python_total/practica_06.py
+++ python_total/practica_06.py
@@ -1,148 +1,101 @@
-# Métodos y ayuda python
+# Abrir, leer y cerrar archivos
 
-cliente = {'nombre': 'Javier', 'apellido': 'Almendro', 'peso': 90, 'altura': 1.86}
-print(cliente.popitem(), cliente.popitem(), cliente)
-
-cadena = ',:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#'
-print(cadena.lstrip(',:%_#'), cadena)
-
-frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
-frutas.insert(3, 'naranja')
-print(frutas)
-
-marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
-marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
-print(marcas_smartphones.isdisjoint(marcas_tv))
-
-# Funciones
-
-def invertir_palabras(palabra):
-    '''
-        Esta función hace dos cosas:
-            - Invierte la palabra
-            - La pone en mayúsculas
-    '''
-    return palabra[::-1].upper()
+mi_archivo = open('data/prueba_practica_006_01.txt')
 print()
-print(invertir_palabras('Javier'))
-
-def todos_pares(lista):
-    for x in lista:
-        if x%2 != 0:
-            return False
-    return True
+print(mi_archivo)
 print()
-print(todos_pares([1,2,15,7,2]))
+print(mi_archivo.read())
+mi_archivo.close()
 
-# Interacción entre funciones
-
-from random import shuffle, choice
-palitos = ['-', '--', '---', '----']
-def mezclar(lista):
-    shuffle(lista)
-    return lista
-def probar_suerte():
-    intento = ''
-    while intento not in ['1', '2', '3', '4']:
-        intento = input('Elige un palito del 1 al 4: ')
-    return int(intento)
-def chequear_intento(lista, intento):
-    if lista[intento -1] == '-':
-        print('A lavar los platos')
-    else:
-        print('Esta vez te has librado')
-    print(f'Te ha tocado el palito {lista[intento -1]}')
-# chequear_intento(mezclar(palitos), probar_suerte())
-
-def reducir_lista(lista_numeros):
-    lista_reducida = list(set(lista_numeros))
-    lista_reducida.pop()
-    return lista_reducida
-def promedio(lista_numeros):
-    suma = 0
-    for x in lista_numeros:
-        suma = suma + x
-    return suma / len(lista_numeros)
+mi_archivo = open('data/prueba_practica_006_01.txt')
 print()
-print(promedio(reducir_lista([1,2,15,7,2])))
+print(mi_archivo.readline().rstrip())  # Quita fin de línea dque viene del archivo
+print(mi_archivo.readline())
+print(mi_archivo.readline())
+mi_archivo.close()
 
-# Argumentos indefinidos *args
-
-def suma(*args):
-    return sum(args)
+mi_archivo = open('data/prueba_practica_006_01.txt')
 print()
-print(suma(1,2,15,7,2))
+print(mi_archivo.readlines())
+print(mi_archivo.readlines())
+mi_archivo.close()
 
-# Argumentos indefinidos **kwargs
-def suma(**kwargs):
-    print(kwargs, kwargs.items())
-    for clave, valor in kwargs.items():
-        print(f'{clave} = {valor}')
-    return sum(kwargs.values())
+# Crear y escribir archivos
+
+mi_archivo = open('data/prueba_practica_006_02.txt', 'w')  # Si no existe, lo crea
+mi_archivo.write('Soy el nuevo texto')
+mi_archivo.close()
+
+mi_archivo = open('data/prueba_practica_006_02.txt', 'w')  # Si existe, lo borra
+mi_archivo.write('Soy el nuevo texto, cuidado que no se ha añadido al anterior')
+mi_archivo.write('Soy el más nuevo sin salto de línea')
+mi_archivo.write('\nYo si llevo salto de línea')
+mi_archivo.close()
+
+mi_archivo = open('data/prueba_practica_006_02.txt', 'a')  # Coloca el cursor al final del fichero
+mi_archivo.write('\nSoy el texto añadido al final')  # Cuidado con salto de línea inicial
+mi_archivo.close()
+
+# Directorios
+
+import os
 print()
-print(suma(x=1, y=2, z=3))
-
-def prueba_todo(num1, num2, *args, **kwargs):
-    print(f'El primer valor es {num1}')
-    print(f'El segundo valor es {num2}')
-    print(f'La suma de los args es {sum(args)}')
-    for clave, valor in kwargs.items():
-        print(f'{clave} = {valor}')
+print(os.getcwd())
+os.chdir('C:\\Users\\jalmen\\Documents\\learning\\python_total\\data')  # Cuidado con barras invertidas
+print(os.getcwd())
+mi_archivo = open('prueba_practica_006_01.txt')
+print(mi_archivo.readlines())
+mi_archivo.close()
+ruta = 'C:\\Users\\jalmen\\Documents\\learning\\python_total\\data\\prueba_practica_006_01.txt'
 print()
-prueba_todo(1,2,3, 4, 5, 6, 7, a=10, b=12, c=14)
+print(os.path.dirname(ruta), os.path.basename(ruta), os.path.split(ruta))
+
+os.chdir('C:\\Users\\jalmen\\Documents\\learning\\python_total\\data')  # Cuidado con barras invertidas
+os.mkdir('otra_data')
+os.rmdir('otra_data')
+
+from pathlib import Path, PureWindowsPath
+directorio = Path('/Users/jalmen/Documents/learning/python_total/data')  # No hace falta C: ni barras invertidas
+archivo = directorio / 'prueba_practica_006_01.txt'  # Cuidado con esta sintaxis
+mi_archivo = open(archivo)
 print()
-args = [3, 4, 5, 6, 7]
-kwargs = {'a': 10, 'b': 12, 'c': 14}
-prueba_todo(1,2,*args, **kwargs)
+print(mi_archivo.readlines())
+mi_archivo.close()
 
-def devolver_distintos(x, y, z):
-    lista = [x, y, z]
-    suma = sum(lista)
-    if suma > 15:
-        return max([x, y, z])
-    elif suma < 10:
-        return min([x, y, z])
-    else:
-        lista.sort()
-        return lista[1]
+# Módulo pathlib
+archivo = Path('C:/Users/jalmen/Documents/learning/python_total/data/prueba_practica_006_01.txt')
 print()
-print(devolver_distintos(4,2,3))
-
-def abcde(palabra):
-    lista = list(set(list(palabra)))
-    lista.sort()
-    return lista
+print(archivo.read_text())
 print()
-print(abcde('entretenido'))
+print(archivo.name, archivo.suffix, archivo.stem)
+if archivo.exists():
+    print('El archivo existe')
+print(PureWindowsPath(archivo))
 
-def doble_cero(*args):
-    for i, x in enumerate(args[:-1]):
-        if x == 0 and args[i+1] == 0:
-            return True
-    return False
+# Path
+
+base = Path.home()
+curso = Path('learning', 'python_total')
+ruta = Path(base, 'Documents', curso, Path('data', 'prueba_practica_006_01.txt'))
 print()
-print(doble_cero(1,2,3,4,5,6,0,0))
-
-def es_primo(n):
-    for i in range(2, n):
-        if (n % i) == 0:
-            return False
-    return True
-def contar_primos(numero):
-    if numero <= 1:
-        return 0
-    else:
-        primos = 0
-        for i in range(2, numero+1):
-            if es_primo(i):
-                primos += 1
-        return primos
+print(ruta)
+print(ruta.parent, ruta.parent.parent)
+ruta2 = ruta.with_name('prueba_practica_006_02.tx')
 print()
-print(contar_primos(17))
+print(ruta2)
+print()
+for i in Path(base, 'Documents', curso).glob('*.py'):
+    print(i)
+print()
+for i in Path(base, 'Documents', curso).glob('**/*.txt'):
+    print(i)
+print()
+print(ruta.relative_to(base))
 
+# Limpiar la consola
 
-
-
-
-
-
+print()
+nombre = input('Dime tu nombre: ')
+edad = input('Dime tu edad: ')
+os.system('cls')
+print(f'Tu nombre es {nombre} y tienes {edad} años.')
--- python_total/proyecto_06.py
+++ python_total/proyecto_06.py
@@ -1,1 +1,94 @@
 # Recetario
+import os
+from pathlib import Path
+
+def seleccion_tarea():
+    seleccion = '0'
+    while seleccion not in [str(x) for x in range(1, 7)]:
+        os.system('cls')
+        print()
+        print('[1] - Leer receta.')
+        print('[2] - Crear receta.')
+        print('[3] - Crear categoría.')
+        print('[4] - Eliminar receta.')
+        print('[5] - Eliminar categoría.')
+        print('[6] - Finalizar programa.')
+        print()
+        seleccion = input('Cúal es tu selección: ')
+    return int(seleccion)
+
+def mostrar_elementos(lista_elementos):
+    print()
+    os.system('cls')
+    for i, v in enumerate(lista_elementos):
+        print(f'[{i + 1}] - {v.name}')
+    print()
+
+
+def seleccion_elemento(ruta, elemento):
+    lista_elementos = list(ruta.glob('*'))
+    if len(lista_elementos) == 0:
+        os.system('cls')
+        print()
+        print('No hay ninguna receta en esta categoría')
+        return ''
+    else:
+        seleccion = '0'
+        while seleccion not in [str(x) for x in range(1, len(lista_elementos) + 1)]:
+            mostrar_elementos(lista_elementos)
+            seleccion = input('Cúal es tu selección: ')
+        return lista_elementos[int(seleccion) - 1]
+
+
+# Init variables
+ruta_categorias = Path(Path.home(), 'Documents', 'learning', 'python_total', 'data', 'proyecto_06')
+seleccion = 1
+while seleccion in range(1, 6):
+    seleccion = seleccion_tarea()
+    if seleccion == 1:
+        categoria = seleccion_elemento(ruta_categorias, 'categoría')
+        ruta_recetas = Path(ruta_categorias, categoria)
+        receta = seleccion_elemento(ruta_recetas, 'receta')
+        if receta != '':
+            ruta_receta = Path(ruta_recetas, receta)
+            os.system('cls')
+            print()
+            print(ruta_receta.read_text())
+        print()
+        input('Presione Enter para siguiente tarea.')
+    elif seleccion == 2:
+        categoria = seleccion_elemento(ruta_categorias, 'categoría')
+        ruta_recetas = Path(ruta_categorias, categoria)
+        lista_recetas = list(ruta_recetas.glob('*'))
+        mostrar_elementos(lista_recetas)
+        nueva_receta = input('Cúal es el nombre de la nueva receta: ')
+        ruta_receta = Path(ruta_recetas, nueva_receta + '.txt')
+        nuevo_fichero = open(ruta_receta, 'w')
+        nuevo_fichero.write(f'Esta es la receta de {nueva_receta}')
+        nuevo_fichero.close()
+    elif seleccion == 3:
+        lista_categorias = list(ruta_categorias.glob('*'))
+        mostrar_elementos(lista_categorias)
+        nueva_categoria = input('Cúal es el nombre de la nueva categoria: ')
+        ruta_categoria = Path(ruta_categorias, nueva_categoria)
+        os.makedirs(ruta_categoria)
+    elif seleccion == 4:
+        categoria = seleccion_elemento(ruta_categorias, 'categoría')
+        ruta_recetas = Path(ruta_categorias, categoria)
+        receta = seleccion_elemento(ruta_recetas, 'receta')
+        if receta != '':
+            ruta_receta = Path(ruta_recetas, receta)
+            os.remove(ruta_receta)
+        else:
+            print()
+            input('Presione Enter para siguiente tarea.')
+    elif seleccion == 5:
+        categoria = seleccion_elemento(ruta_categorias, 'categoría')
+        ruta_recetas = Path(ruta_categorias, categoria)
+        for receta in list(ruta_recetas.glob('*')):
+            os.remove(receta)
+        os.rmdir(ruta_recetas)
+
+
+
+
